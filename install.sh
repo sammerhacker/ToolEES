717a7acc613585531b9d7fcf3e2229fc196ae5c3
sayser <68947652+sammerhacker@users.noreply.github.com>
2022-02-15 12:19:45 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+clear
+echo "admin : DEV SAY SER";
+echo " ";
+pkg update -y
+pkg upgrade -y
+pkg install ruby -y
+pkg install sl -y
+pkg install python -y
+pkg install figlet -y
+pkg install figlet -y
+pkg install toilet -y
+pip install requests
+pip install stdiomask
+pkg install ruby -y
+gem install lolcat
+pip install cfscrape
+pip install pyfiglet
+pip install cowsay
